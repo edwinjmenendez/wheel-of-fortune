137fefa52860921ca74f34b687ba947578e858fd
Edwin M <edwinjmenendez@gmail.com>
2023-08-30 00:47:02 -0700

    added routers to application

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Routes, Outlet, Link } from 'react-router-dom';
 import Wheel from './components/Wheel/Wheel'; // Import your Wheel component
 import PreferenceForm from './components/preferenceForm/PreferenceForm';
+import FoodList from './components/foodlist/FoodList';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Wheel of Fortune App</h1>
-      <PreferenceForm />
-      {/* <Wheel /> */}
-    </div>
-  );
+    const [foodData, setFoodData] = useState({});
+    const [foodSelection, setFoodSelection] = useState({});
+    return (
+        <div>
+            <h1>Wheel of Fortune App</h1>
+            <Routes>
+                <Route path='/' element={<PreferenceForm setFoodData={setFoodData} />} />
+                {foodData.allFoods && foodData.budget ? (
+                    <Route path='wheel' element={<Wheel setFoodSelection={setFoodSelection} foodData={foodData} />} />
+                ) : null}
+                <Route path='foodlist' element={<FoodList foodSelection={foodSelection} />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { 
+    createBrowserRouter,
+    RouterProvider,
+    BrowserRouter
+ } from 'react-router-dom';
 import App from './App'; // Import your App component
 import './index.css'; // Import your CSS if needed
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello World! </div>
+    }
+])
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App /> 
+    </BrowserRouter>,
+    document.getElementById('root')
+);
