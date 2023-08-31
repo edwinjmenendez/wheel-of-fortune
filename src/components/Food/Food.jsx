8404678df0260f0c1834b1932e59a9a15b2b9c95
Edwin M <edwinjmenendez@gmail.com>
2023-08-31 00:10:48 -0700

    added some simple styling

--- src/components/Food/Food.jsx
+++ src/components/Food/Food.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
+import React from 'react';
+import './Food.css'; // Import your CSS file for styling
 
 const Food = ({ business }) => {
+  const {
+    id,
+    image_url,
+    is_closed,
+    location,
+    name,
+    phone,
+    price,
+    rating,
+    review_count,
+    url,
+  } = business;
 
-    const {
-        id,
-        image_url,
-        is_closed,
-        location,
-        name, phone,
-        price, rating,
-        review_count,
-        url } = business;
-    
-        const { address1, city, state, zip_code } = location
+  const { address1, city, state, zip_code } = location;
 
-    return (
-        <div id={id}>
-            <a href={url}>
-                <img src={image_url} />
-            </a>
-            <h2>Name: {name}</h2>
-            <p>Phone: {phone}</p>
-            <p>Price: {price}</p>
-            <p>rating: {rating}</p>
-            <p>Review: {review_count}</p>
-            <p>Currently { is_closed ? 'closed' : 'open' }</p>
-            <p>Address {address1}</p>
-            <p>{city} {state} {zip_code}</p>
-
+  return (
+    <div className="food-container" id={id}>
+      <a className="food-image-link" href={url}>
+        <img className="food-image" src={image_url} alt={name} />
+      </a>
+      <div className="food-details">
+        <h2 className="food-name">
+          <a href={url}>{name}</a>
+        </h2>
+        <p className="food-phone">Phone: {phone}</p>
+        <p className="food-price">Price: {price}</p>
+        <p className="food-rating">Rating: {rating}</p>
+        <p className="food-review-count">Reviews: {review_count}</p>
+        <p className="food-status">
+          Currently {is_closed ? 'Closed' : 'Open'}
+        </p>
+        <div className="food-address">
+          <p className="food-address-line">{address1}</p>
+          <p className="food-address-line">
+            {city}, {state} {zip_code}
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Food
+export default Food;
