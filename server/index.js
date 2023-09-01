da2d71ac242f7db4d017c26aa09de96560206548
Edwin M <edwinjmenendez@gmail.com>
2023-09-01 13:24:12 -0700

    served the frontend pages with my backend

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const path = require('path');
 
 const YELP_API_URL = 'https://api.yelp.com/v3/autocomplete';
 const YELP_API_URL2 = 'https://api.yelp.com/v3/businesses/search?';
 app.use(express.json());
 app.use(cors())
+
+app.use(express.static('dist'));
 // Proxy route
 app.get('/autocomplete', async (req, res) => {
   try {
@@ -38,6 +41,25 @@ app.get('/results', async (req, res) => {
     res.status(500).json({ error: 'An error occurred' });
   }
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..','dist', 'index.html'));
+});
+
+const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err)
+  }
+
+  res.status(500).json({
+    success: false,
+    error: err.message || 'Server Error',
+    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
+  })
+}
+
+// Error handler middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
